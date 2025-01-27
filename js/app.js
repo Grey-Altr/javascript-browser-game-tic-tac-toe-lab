83a450967f496af25007a25ee21ce1ee4cb13e50
GreyA <grey.altaer@gmail.com>
2025-01-27 10:21:54 -0800

    querySelectors added to app.js

--- js/app.js
+++ js/app.js
@@ -21,7 +21,8 @@ let turn;
 let winner;
 let tie;
 /*------------------------ Cached Element References ------------------------*/
-
+const squareEls = document.querySelectorAll('#sqr');
+const messageEl = document.querySelector('#message');
 /*-------------------------------- Functions --------------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
